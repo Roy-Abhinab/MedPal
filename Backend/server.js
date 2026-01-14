6dd2c6e489fcae76adc84bcfb1cac504baca3c10
Abhinab Roy <133621842+Roy-Abhinab@users.noreply.github.com>
2026-01-14 19:59:20 +0530

    Setup MongoDB connection, cloudinary config, and user/doctor models.

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import e from 'express'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 const app = express()
 const port = process.env.PORT || 7000
+connectDB()
+connectCloudinary()
 
 app.use(express.json())
 app.use(cors())
