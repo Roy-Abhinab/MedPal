0282283b0d042bd7e82527421aeb13dee9b7087d
Abhinab Roy <133621842+Roy-Abhinab@users.noreply.github.com>
2026-01-16 19:17:00 +0530

    Added pakages for admin and created admin login page.

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { addDoctor } from '../controllers/adminControl.js';
+import { addDoctor, loginAdmin } from '../controllers/adminControl.js';
 import upload from '../middlewares/multer.js';
+import authAdmin from '../middlewares/authAdmin.js';
 
 const adminArouter = express.Router();
 
-adminArouter.post('/add-doctor', upload.single('image'), addDoctor);
+adminArouter.post('/add-doctor', authAdmin, upload.single('image'), addDoctor);
+adminArouter.post('/login', loginAdmin);
 
 export default adminArouter;
