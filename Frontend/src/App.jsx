9ee672a22b86d260c359fa79df42f92c6efa3fbb
Abhinab Roy <133621842+Roy-Abhinab@users.noreply.github.com>
2026-01-19 02:28:18 +0530

    Created User login functionalities created APIs for User data updating and fetching.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,10 +10,13 @@ import Appointments from './pages/Appointments'
 import MyAppointment from './pages/MyAppointment'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
